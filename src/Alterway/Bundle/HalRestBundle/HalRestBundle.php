8d4900f0fcba46ddd4288143f54faf106c922b40
Halleck45 <jeanfrancois@lepine.pro>
2013-04-30 15:10:31 +0200

    projet initialisé

---
+++ src/Alterway/Bundle/HalRestBundle/HalRestBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Alterway\Bundle\HalRestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/*
+ * (c) 2013 Alter Way
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class HalRestBundle extends Bundle
+{
+    
+}
---
+++ test/behavior/Fixtures/AwHalRestBundle/app/autoload.php
@@ -0,0 +1,4 @@
+<?php 
+die('ici');
+$loader = require(__DIR__.'/../../vendor/autoload.php');
+$loader->add('Alterway\\Bundle\\HalRestBundle', __DIR__.'/../../../src');
