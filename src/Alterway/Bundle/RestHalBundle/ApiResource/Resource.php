4ee779bb497d628c34a9da97aae0b6aae87b4337
yacinebenfaid <benfaid.yacine@gmail.com>
2013-10-15 11:23:01 +0200

    add link

--- src/Alterway/Bundle/RestHalBundle/ApiResource/Resource.php
+++ src/Alterway/Bundle/RestHalBundle/ApiResource/Resource.php
@@ -5,7 +5,6 @@ namespace Alterway\Bundle\RestHalBundle\ApiResource;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 
-
 abstract class Resource implements ResourceInterface
 {
     /**
@@ -26,15 +25,8 @@ abstract class Resource implements ResourceInterface
 
     public function addLink($rel, $route, array $routeParams = array(), array $attributes = array())
     {
-        $generatedRoute = $this->generate($route, $routeParams);
-        $methods = $this->router->getRouteCollection()->get($route)->getMethods();
-        $this->hal->addLink(
-            $rel,
-            $generatedRoute,
-            null,
-            array_merge($attributes, array('method' => reset($methods)))
-        );
-
+        $this->hal->addLink($rel, $this->generate($route, $routeParams), null,
+            array_merge($attributes, array('method' => reset($this->router->getRouteCollection()->get($route)->getMethods()))));
         return $this;
     }
 
