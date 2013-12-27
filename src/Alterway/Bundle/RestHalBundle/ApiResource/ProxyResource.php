c70aabf3ae0b285e121bb57599b071f1f828318a
Benoît Merlet <benoit.merlet@alterway.fr>
2013-12-27 15:55:26 +0100

    Fixed compat with nocarrier/hal v0.9.4

--- src/Alterway/Bundle/RestHalBundle/ApiResource/ProxyResource.php
+++ src/Alterway/Bundle/RestHalBundle/ApiResource/ProxyResource.php
@@ -6,17 +6,4 @@ use Nocarrier\Hal;
 
 class ProxyResource extends Hal
 {
-
-    public function setData(array $array)
-    {
-        $this->data = $array;
-        return $this;
-    }
-
-    public function setUri($uri)
-    {
-        $this->uri = $uri;
-        return $this;
-    }
-
 }
