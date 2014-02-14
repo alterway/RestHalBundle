8063dd5ac4d5e4182351665c9f8b4f75fa7a5640
yacine <yacine@lrqdo.fr>
2014-02-14 19:10:39 +0100

    Add getResource in HalResponse

--- src/Alterway/Bundle/RestHalBundle/Response/HalResponse.php
+++ src/Alterway/Bundle/RestHalBundle/Response/HalResponse.php
@@ -7,10 +7,19 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HalResponse extends Response implements HalResponseInterface
 {
+    private $resource;
+
     public function __construct(ResourceInterface $resource, $status = 200)
     {
         parent::__construct($resource->asJson(), $status, array(
             'Content-Type' => 'application/hal+json',
         ));
+
+        $this->resource = $resource;
+    }
+
+    public function getResource()
+    {
+        return $this->resource;
     }
 }
